48d81439147ef67681219827bc2750ca5ea6940f
Marcelo <mrclgst10@gmail.com>
2022-03-07 08:27:29 -0300

    implement select and orderby methods

--- src/fluentSQL.js
+++ src/fluentSQL.js
@@ -1,70 +1,98 @@
 export default class FluentSQlBuilder {
-  #database = []
-  #limit = 0
-  #select = []
-  #where = []
-  #orderBy = ''
+  #database = [];
+  #limit = 0;
+  #select = [];
+  #where = [];
+  #orderBy = '';
 
   constructor({ database }) {
-    this.#database = database
+    this.#database = database;
   }
 
   static for(database) {
-    return new FluentSQlBuilder({ database })
+    return new FluentSQlBuilder({ database });
   }
 
   limit(max) {
-    this.#limit = max
+    this.#limit = max;
 
-    return this
+    return this;
   }
 
   select(props) {
-    this.#select = props
+    this.#select = props;
 
-    return this
+    return this;
   }
 
   where(query) {
     /**
      * This will transform an object like { key: 'value' } on an array like [['key', 'value']]
      */
-    const [[prop, value]] = Object.entries(query)
-    const whereFilter = value instanceof RegExp ? value : new RegExp(value)
+    const [[prop, value]] = Object.entries(query);
+    const whereFilter = value instanceof RegExp ? value : new RegExp(value);
 
-    this.#where.push({ prop, filter: whereFilter })
+    this.#where.push({ prop, filter: whereFilter });
 
-    return this
+    return this;
   }
 
   orderBy(field) {
-    this.#orderBy = field
+    this.#orderBy = field;
 
-    return this
+    return this;
   }
 
   #performWhere(item) {
     for (const { filter, prop } of this.#where) {
-      if (!filter.test(item[prop])) return false
+      if (!filter.test(item[prop])) return false;
     }
 
-    return true
+    return true;
   }
 
   #performLimit(results) {
-    return this.#limit && results.length === this.#limit
+    return this.#limit && results.length === this.#limit;
+  }
+
+  #performSelect(item) {
+    const mappedItem = {};
+    const entries = Object.entries(item);
+
+    for (const [key, value] of entries) {
+      if (this.#select.length && !this.#select.includes(key)) continue;
+
+      mappedItem[key] = value;
+    }
+
+    return mappedItem;
+  }
+
+  #performOrderBy(results) {
+    if (!this.#orderBy) return results;
+
+    return results.sort((prev, next) => {
+      return prev[this.#orderBy]
+        .toString()
+        .localeCompare(next[this.#orderBy].toString());
+    });
   }
 
   build() {
-    const results = []
+    const results = [];
+
     for (const item of this.#database) {
-      if (!this.#performWhere(item)) continue
+      if (!this.#performWhere(item)) continue;
 
-      results.push(item)
+      const mappedItem = this.#performSelect(item);
 
-      if (this.#performLimit(results)) break
+      results.push(mappedItem);
+
+      if (this.#performLimit(results)) break;
     }
 
-    return results
+    const orderedResults = this.#performOrderBy(results);
+
+    return orderedResults;
   }
 }
